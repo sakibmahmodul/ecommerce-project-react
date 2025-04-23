7a3e3a61faf80d808132c743f765d0d0924418c7
sakibmahmodul <sakibuddessho89@gmail.com>
2025-04-23 23:22:41 +0600

    Work with form & Footer Section

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -33,7 +33,23 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper></Wrapper>;
-};
+  return(
+    <Wrapper>
+      <h2 className="common-heading">Contact Page</h2>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3659.896102759694!2d91.187379576017!3d23.464211899701787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x37547f37029faff5%3A0x7d88a45fa7cb3ad0!2z4Kao4Ka_4KaJIOCmpOCmleCmpuCngOCmsCDgpqvgpr7gprDgp43gpqPgpr_gpprgpr7gprA!5e0!3m2!1sbn!2sbd!4v1745415452892!5m2!1sbn!2sbd" 
+      width="100%" height="400" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+      <div className="container">
+        <div className="contact-form">
+          <form action="https://formspree.io/f/xdkgoboz" method="POST" className="contact-inputs">
+            <input type="text" placeholder="username" name="username" required autoComplete="off"/>
+            <input type="email" name="Email" placeholder="Email" required />
+            <textarea name="messages" placeholder="Enter Your Messages" required autoComplete="off" cols="30" rows="10"></textarea>
+            <input type="submit" value="Send" />
+          </form>
+        </div>
+      </div>
+  </Wrapper>
+
+  )};
 
 export default Contact;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import HeroSection from './components/HeroSection'
+import Services from './components/Services'
+import Trusted from './components/Trusted'
 
 const Home = () => {
   const data = {
     name: "Thapa Store"
   }
   return (
-    <HeroSection myData={data}/>
+    <>
+      <HeroSection myData={data}/>
+      <Services/>
+      <Trusted/>
+    </>
+    
   )
 }
 
