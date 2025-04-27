99ac9671c5e3be2c9a9ad490f2783ad283fb794a
sakibmahmodul <sakibuddessho89@gmail.com>
2025-04-28 01:21:16 +0600

    Product, Featured Product Section

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AppProvider } from './components/context/productcontext.jsx'
 //import reportWebVitals from "./reportWebVitals";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </StrictMode>
   )
 // If you want to start measuring performance in your app, pass a function
